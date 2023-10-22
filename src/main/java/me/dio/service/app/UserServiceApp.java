7d658049cdd911d13bff6818bb87bc131cb2ebb3
gjmb <gjmbarbosa@gmail.com>
2023-10-22 20:33:28 -0300

    add service class

---
+++ src/main/java/me/dio/service/app/UserServiceApp.java
@@ -0,0 +1,31 @@
+package me.dio.service.app;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceApp implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceApp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
